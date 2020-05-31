9cfebd8fcbe22394bb3858ebacaed42bf24c2a02
alanwagner <mail@alanwagner.org>
2020-05-31 13:17:21 +0200

    Refactor php functions

---
+++ inc/reader.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * WhoNews : reader.php
+ *
+ * @author     Alan G. Wagner <mail@alanwagner.org>
+ * @copyright  2020 Alan G. Wagner
+ * @license    GNU GPL 3.0
+ */
+
+/**
+ * Read XML feed data from url and return parsed as php array
+ *
+ * @param string $url
+ * @param int $limit
+ * @return array
+ */
+function getChannelData($url, $limit=null) {
+    $data = [];
+    $content = null;
+
+    $ch = curl_init();
+
+    ob_start();
+
+    // Configuration de l'URL et d'autres options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    // Récupération de l'URL
+    curl_exec($ch);
+
+    // Fermeture de la session cURL
+    curl_close($ch);
+
+    $content = ob_get_contents();
+    ob_end_clean();
+
+    if (is_string($content)) {
+
+        $data = parseChannelContent($content, $limit);
+    }
+
+    return $data;
+}
+
+/**
+ * Parse a channel's XML string into a PHP array
+ *
+ * Return is of the form:
+ *   title : string
+ *   items : array (guid, title, description, pubDate[, imgUrl, thumbUrl])
+ *
+ * @param string $content
+ * @param int $limit
+ * @return array
+ */
+function parseChannelContent($content, $limit=null)
+{
+    $xml=simplexml_load_string($content);
+    $channel = $xml->channel;
+
+    $data['title'] = $channel->title;
+    $data['items'] = [];
+
+    foreach($channel->item as $item) {
+
+        $itemData = [];
+
+        $guid = $item->guid;
+        if (!is_string($guid)) {
+            $guid = $guid->__toString();
+        }
+        $itemData['guid'] = strip_tags($guid);
+
+        $title = $item->title;
+        if (!is_string($title)) {
+            $title = $title->__toString();
+        }
+        $itemData['title'] = strip_tags($title);
+
+        $description = $item->description;
+        if (!is_string($description)) {
+            $description = $description->__toString();
+        }
+        if (strstr($description, '<')) {
+            //  Clean tags and embedded links
+            $description = strip_tags(substr($description, 0, strpos($description, '<', 10)));
+        }
+        $itemData['description'] = $description;
+
+        $pubTime = strtotime($item->pubDate);
+        if ($pubTime > 1000000000) {
+            //  Format 2020.05.31 11:11 GMT
+            $itemData['pubDate'] = sprintf('%s GMT', gmdate('Y.m.d H:i', $pubTime));
+        }
+
+
+        $media = $item->children('http://search.yahoo.com/mrss/');
+
+        if (isset($media->group->content)) {
+
+            //  Fox
+            $attrs = $media->group->content->attributes();
+            $itemData['imgUrl'] = strval($attrs['url']);
+
+            //print_r($media->group->content->attributes());
+
+        } else if (isset($media->content)) {
+
+
+            //  NYT
+            $attrs = $media->content->attributes();
+            $itemData['imgUrl'] = strval($attrs['url']);
+
+            //print_r($media->content->attributes());
+        }
+
+        if (isset($media->thumbnail)) {
+
+            //  BuzzFeed only has thumbnail
+            $attrs = $media->thumbnail->attributes();
+            $itemData['thumbUrl'] = strval($attrs['url']);
+        }
+
+
+        $data['items'][] = $itemData;
+
+        if (!empty($limit) && count($data['items']) >= $limit) {
+            break;
+        }
+    }
+
+    return $data;
+}
