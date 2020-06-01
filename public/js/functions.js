7ccd2d01979d3f01904b8ba36223de0299bef291
alanwagner <mail@alanwagner.org>
2020-06-01 20:39:10 +0200

    Added file docblock

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,3 +1,11 @@
+/**
+ * WhoNews : functions.js
+ *
+ * @author     Alan G. Wagner <mail@alanwagner.org>
+ * @copyright  2020 Alan G. Wagner
+ * @license    GNU GPL 3.0
+ */
+
 function toggleSettings() {
   var element = document.getElementById("wn-settings-wrapper");
   element.classList.toggle("wn-settings-hidden");
